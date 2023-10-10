b0a8061968be770ade4a6f921fdf0749317fd905
Sam Rowden <sam@ricorocks.agency>
2023-10-10 16:41:40 +0100

    Clearer tests

--- tests/Feature/RenderTest.php
+++ tests/Feature/RenderTest.php
@@ -1,12 +1,22 @@
 <?php
 
-test('pages without livewire render', function () {
+test('tests with view assertions function with pages without livewire', function () {
     $this->get('/without')
         ->assertViewIs('without-livewire')
         ->assertSee('Hey!');
 });
 
-test('pages with livewire render', function () {
+test('tests without view assertions function with pages without livewire', function () {
+    $this->get('/without')
+        ->assertSee('Hey!');
+});
+
+test('tests without view assertions function when pages have livewire', function () {
+    $this->get('/with')
+        ->assertSee('Hey!');
+});
+
+test('tests with view assertions function when pages have livewire', function () {
     $this->get('/with')
         ->assertViewIs('with-livewire')
         ->assertSee('Hey!');
